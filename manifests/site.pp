88d0c1bb91a6681466a4b60bbc0bcc8cbec96f83
root <root@puppet.kandang.com>
2022-08-03 02:08:11 +0100

    wip

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,5 @@
 node 'puppet.kandang.com' {
-#  include 'baseline'
-$test = hiera('test')
-$secret_var = lookup('secret_var')
- notify {"We aren\'t setup to configure and value of test is $secret_var)":}
+  include 'baseline'
 }
 
 node 'agent1.kandang.com' {
