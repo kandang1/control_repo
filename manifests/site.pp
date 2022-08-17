497a76c633b32b55b04cfc2b2c4eacfe5a6f03e6
root <root@puppet.kandang.com>
2022-08-17 02:54:36 +0100

    wip

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,9 @@
 node 'puppet.kandang.com' {
-  include baseline
 }
 
 node 'agent1.kandang.com' {
-  include baseline
-  include profiles
 }
 
 node default {
- notify {'We aren\'t setup to configure anything':}
+  include baseline
 }
